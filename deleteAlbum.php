745423cc72e4ecfd3a2d5b899a4d1dcc3d4018fd
lorsie <152486611+lorsie@users.noreply.github.com>
2024-12-06 22:52:04 +0800

    Create deleteAlbum.php

---
+++ deleteAlbum.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'core/dbConfig.php';
+require_once 'core/models.php';
+
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php");
+}
+
+$album_id = $_GET['album_id'];
+$album = getAlbumByID($pdo, $album_id);
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Delete Album</title>
+    <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+    <?php include 'navbar.php'; ?>
+    <div class="deleteAlbumForm" style="display: flex; justify-content: center;">
+        <form action="core/handleForms.php" method="POST">
+            <p>
+                <label for=""><h2>Are you sure you want to Delete this Album?</h2></label>
+                <input type="hidden" name="album_id" value="<?php echo $album_id; ?>">
+                <input type="submit" name="deleteAlbumBtn" style="margin-top: 10px;" value="Delete Album">
+            </p>
+        </form>
+    </div>
+</body>
+</html>
