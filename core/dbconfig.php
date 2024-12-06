d33575b2960f33d79be8aee03f2c2cc4b14191a8
lorsie <152486611+lorsie@users.noreply.github.com>
2024-12-06 23:27:52 +0800

    Update dbconfig.php

--- core/dbconfig.php
+++ core/dbconfig.php
@@ -3,7 +3,7 @@ session_start();
 $host = "localhost";
 $user = "root";
 $password = "";
-$dbname = "hernandez";
+$dbname = "memories";
 $dsn = "mysql:host={$host};dbname={$dbname}";
 
 $pdo = new PDO($dsn,$user,$password);
